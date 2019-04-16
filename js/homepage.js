678f59f2d0dfd1c343973dd77cd9e90e1668daa8
Carlos <carl7755@easv365.dk>
2019-04-16 18:00:54 +0200

    init commit

---
+++ js/homepage.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+
